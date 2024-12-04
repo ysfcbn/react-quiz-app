47f0a275b7a742ae3ca4c34e7eee591c630873c1
Yusuf Çoban <pyusuf24@gmail.com>
2024-12-04 23:23:54 +0300

    add progress and answer

--- src/components/App.js
+++ src/components/App.js
@@ -5,11 +5,15 @@ import Error from "./Error";
 import Loader from "./Loader";
 import Question from "./Question";
 import StartScreen from "./StartScreen";
+import NextButton from "./NextButton";
+import Progress from "./Progress";
 
 const initialState = {
   questions: [],
   status: "loading",
   index: 0,
+  answer: null,
+  points: 0,
 };
 
 function reducer(state, action) {
@@ -30,18 +34,41 @@ function reducer(state, action) {
         ...state,
         status: "active",
       };
+    case "newAnswer":
+      const question = state.questions.at(state.index);
+
+      return {
+        ...state,
+        answer: action.payload,
+        points:
+          action.payload === question.correctOption
+            ? state.points + question.points
+            : state.points,
+      };
+
+    case "nextQuestion":
+      return {
+        ...state,
+        index: state.index++,
+        answer: null,
+      };
     default:
       throw new Error("Action unknown");
   }
 }
 
 function App() {
-  const [{ questions, status, index }, dispatch] = useReducer(
+  const [{ questions, status, index, answer, points }, dispatch] = useReducer(
     reducer,
     initialState
   );
 
   const numQuestions = questions.length;
+  const maxPoissiblePoints = questions.reduce(
+    (prev, cur) => prev + cur.points,
+    0
+  );
+
   useEffect(function () {
     fetch("http://localhost:9000/questions")
       .then((res) => res.json())
@@ -58,7 +85,23 @@ function App() {
         {status === "ready" && (
           <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
         )}
-        {status === "active" && <Question question={questions[index]} />}
+        {status === "active" && (
+          <>
+            <Progress
+              index={index}
+              numQuestions={numQuestions}
+              points={points}
+              maxPoissiblePoints={maxPoissiblePoints}
+              answer={answer}
+            />
+            <Question
+              question={questions[index]}
+              dispatch={dispatch}
+              answer={answer}
+            />
+            <NextButton dispatch={dispatch} answer={answer} />
+          </>
+        )}
       </Main>
     </div>
   );
--- src/components/Options.js
+++ src/components/Options.js
@@ -1,11 +1,31 @@
-function Options({ question }) {
+function Options({ question, dispatch, answer }) {
+  const hasAnswewered = answer !== null;
+
   return (
     <div className="options">
-      {question.options.map((option) => (
-        <button className="btn btn-option" key={option}>
-          {option}
-        </button>
-      ))}
+      {question.options.map((option, index) => {
+        const isSelected = index === answer;
+        const isCorrect = index === question.correctOption;
+
+        // Sınıf adlarını dinamik olarak belirleyin
+        const buttonClass = `
+          btn btn-option 
+          ${isSelected ? "answer" : ""} 
+          ${hasAnswewered && isCorrect ? "correct" : hasAnswewered && "other"} 
+          ${hasAnswewered && isSelected && !isCorrect ? "wrong" : ""}
+        `.trim();
+
+        return (
+          <button
+            className={buttonClass}
+            key={option}
+            disabled={hasAnswewered}
+            onClick={() => dispatch({ type: "newAnswer", payload: index })}
+          >
+            {option}
+          </button>
+        );
+      })}
     </div>
   );
 }
